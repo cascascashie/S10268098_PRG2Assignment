4b2b9f8436005d2fdc11646dea943fda4c64a431
Casandra <s10268098@connect.np.edu.sg>
2025-01-18 20:45:09 +0800

    Partially did method 1 (load airlines and boarding gates)

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -4,10 +4,12 @@
 
 // METHOD 1 - Load files (airlines and boarding gates) [CASANDRA] 
 
+using Assignment2;
+
 void LoadAirlinesAndBoardingGates()
 {
     // LOADING AIRLINES
-    string[] airlinesData = File.ReadAllLines("airlines.csv"); // ALL OF THE AIRLINES (COUNTRY, AIRLINES CODE)
+    string[] airlinesData = File.ReadAllLines("airlines.csv"); // ALL OF THE AIRLINES (AIRLINE, AIRLINES CODE)
 
     foreach (string data in airlinesData)
     {
@@ -18,145 +20,184 @@ void LoadAirlinesAndBoardingGates()
 
         else
         {
-            string[] airlinesDetails = data.Split(','); // SPECIFIC DETAILS FOR ONE AIRLINES (SPECIFIC COUNTRY, AIRLINES CODE}
-            Console.WriteLine(airlinesDetails[0] + ", AND THE AIRLINES CODE IS " + airlinesDetails[1]);
+            string[] airlinesDetails = data.Split(','); // SPECIFIC DETAILS FOR ONE AIRLINES (SPECIFIC AIRLINE, AIRLINES CODE}
+
+            // NOTE 
+            // DATA [0] REPRESENTS THE AIRLINES NAME
+            // DATA [1] REPRESENTS THE AIRLINES CODE 
+
+            // NEED TO WAIT FOR HT TO MAKE THE AIRLINES CLASS
+
+            // MAKING THE NEW AIRLINES OBJECTS 
+            // Airline airline = new Airline(data[0], data[1]); - UNCOMMENT ONCE HT CREATES THE AIRLINES CLASS !!
+            //Airline.Flights.Add(data[0], airline); - UNCOMMENT ONCE HT CREATES THE AIRLINES CLASS !!
+        }
+    }
+
+    // LOADING BOARDING GATES 
+    string[] boardingGatesData = File.ReadAllLines("boardinggates.csv");
+
+    foreach (string data in boardingGatesData)
+    {
+        if (data == boardingGatesData[0])
+        {
+            continue;
+        }
+        else
+        {
+            Console.WriteLine(data);
+            Console.WriteLine("");
+
+            string[] boardingGatesDetails = data.Split(",");
+
+            // NOTE
+            // DATA[0] REPRESENTS BOARDING GATE 
+            // DATA[1] REPRESENTS DDJB
+            // DATA[2] REPRESENTS CFFT
+            // DATA[3] REPRESENTS LWTT
+
+            // NEED TO WAIT FOR HT TO MAKE BOARDINGGATE CLASS
+
+            // MAKING NEW BOARDINGGATE OBJECTS 
+            // BoardingGate boardinggate = new BoardingGate(boardingGatesDetails[0], boardingGatesDetails[1], boardingGatesDetails[2], boardingGatesDetails[3]); 
+            // ^^ - UNCOMMENT ONCE HT CREATES THE BOARDINGGATE CLASS !!
+
+            //Terminal.BoardingGates.Add(data[0], boardinggate); - UNCOMMENT ONCE HT CREATES BOARDINGGATE AND TERMINAL CLASSES
         }
     }
 }
 
-LoadAirlinesAndBoardingGates();
 
+    // METHOD 2 - Load files (flights) [HNIN THAW]
 
-
-// METHOD 2 - Load files (flights) [HNIN THAW]
-
-void LoadFlights()
-{
-    // CAN ADJUST TO HOWEVER YOU WANT 
-}
-
-
-
-
-// METHOD 3 - List all flights with basic information [HNIN THAW]
-
-void ListFlightDetails()
-{
-    // CAN ADJUST TO HOWEVER YOU WANT 
-}
+    void LoadFlights()
+    {
+        // CAN ADJUST TO HOWEVER YOU WANT 
+    }
 
 
 
 
-// METHOD 4 - List all boarding gates [CASANDRA] 
+    // METHOD 3 - List all flights with basic information [HNIN THAW]
 
-void ListBoardingGates()
-{
-    // CAN ADJUST TO HOWEVER YOU WANT 
-}
-
-
-
-
-// METHOD 5 - Assign a boarding gate to a flight [HNIN THAW]
-
-void AssignBoardingGateToFlight()
-{
-    // CAN ADJUST TO HOWEVER YOU WANT 
-}
+    void ListFlightDetails()
+    {
+        // CAN ADJUST TO HOWEVER YOU WANT 
+    }
 
 
 
 
-// METHOD 6 - Create a new flight [HNIN THAW]
+    // METHOD 4 - List all boarding gates [CASANDRA] 
 
-void CreateFlight()
-{
-    // CAN ADJUST TO HOWEVER YOU WANT 
-}
-
-
-
-
-// METHOD 7 - Display full flight details from an airline [CASANDRA] 
-
-void FullFlightDetail()
-{
-    // CAN ADJUST TO HOWEVER YOU WANT 
-}
+    void ListBoardingGates()
+    {
+        // CAN ADJUST TO HOWEVER YOU WANT 
+    }
 
 
 
 
-// METHOD 8 - Modify flight details [CASANDRA] 
+    // METHOD 5 - Assign a boarding gate to a flight [HNIN THAW]
 
-void ModifyFlightDetails()
-{
-    // CAN ADJUST TO HOWEVER YOU WANT 
-}
-
-
+    void AssignBoardingGateToFlight()
+    {
+        // CAN ADJUST TO HOWEVER YOU WANT 
+    }
 
 
-// METHOD 9 - Display scheduled flights in chronological order, where boarding gates are applicable [HNIN THAW]
 
-void DisplayFlights()
-{
-    // CAN ADJUST TO HOWEVER YOU WANT 
-}
+
+    // METHOD 6 - Create a new flight [HNIN THAW]
+
+    void CreateFlight()
+    {
+        // CAN ADJUST TO HOWEVER YOU WANT 
+    }
+
+
+
+
+    // METHOD 7 - Display full flight details from an airline [CASANDRA] 
+
+    void FullFlightDetail()
+    {
+        // CAN ADJUST TO HOWEVER YOU WANT 
+    }
+
+
+
+
+    // METHOD 8 - Modify flight details [CASANDRA] 
+
+    void ModifyFlightDetails()
+    {
+        // CAN ADJUST TO HOWEVER YOU WANT 
+    }
+
+
+
+
+    // METHOD 9 - Display scheduled flights in chronological order, where boarding gates are applicable [HNIN THAW]
+
+    void DisplayFlights()
+    {
+        // CAN ADJUST TO HOWEVER YOU WANT 
+    }
 
 
 
 
 
-// ADVANCED FEATURES [20% INDIVIDUAL]
+    // ADVANCED FEATURES [20% INDIVIDUAL]
 
-// METHOD (a) - Process all unassigned flights to boarding gates in bulk
+    // METHOD (a) - Process all unassigned flights to boarding gates in bulk
 
-void ProcessUnassignedFlights()
-{
-    // CAN ADJUST TO HOWEVER YOU WANT 
-}
-
-
-
-
-// METHOD (b) - Display the total fee per airline for the day
-
-void DisplayTotalFee()
-{
-    // CAN ADJUST TO HOWEVER YOU WANT 
-}
+    void ProcessUnassignedFlights()
+    {
+        // CAN ADJUST TO HOWEVER YOU WANT 
+    }
 
 
 
 
-// OTHER METHODS
+    // METHOD (b) - Display the total fee per airline for the day
 
-// MAIN MENU METHOD 
-
-void MainMenu()
-{
-    Console.WriteLine("=============================================");
-    Console.WriteLine("Welcome to Changi Airport Terminal 5");
-    Console.WriteLine("=============================================");
-    Console.WriteLine("1. List All Flights");
-    Console.WriteLine("2. List Boarding Gates");
-    Console.WriteLine("3. Assign a Boarding Gate to a Flight");
-    Console.WriteLine("4. Create Flight");
-    Console.WriteLine("5. Display Airline Flights");
-    Console.WriteLine("6. Modify Flight Details");
-    Console.WriteLine("7. Display Flight Schedule");
-    Console.WriteLine("0. Exit");
-    Console.WriteLine("Please select your option: ");
-}
+    void DisplayTotalFee()
+    {
+        // CAN ADJUST TO HOWEVER YOU WANT 
+    }
 
 
 
 
-// MAIN LOOP 
+    // OTHER METHODS
 
-// NOTE FOR FUTURE REFERENCE : 
-// WHEN LOADING THE FILES, NEED TO PUT "FINISHED LOADING AND DISPLAY QUANTITY OF THINGS LOADED, THEN HAVE A GAP
-// BEFORE THE MAIN MENU 
+    // MAIN MENU METHOD 
+
+    void MainMenu()
+    {
+        Console.WriteLine("=============================================");
+        Console.WriteLine("Welcome to Changi Airport Terminal 5");
+        Console.WriteLine("=============================================");
+        Console.WriteLine("1. List All Flights");
+        Console.WriteLine("2. List Boarding Gates");
+        Console.WriteLine("3. Assign a Boarding Gate to a Flight");
+        Console.WriteLine("4. Create Flight");
+        Console.WriteLine("5. Display Airline Flights");
+        Console.WriteLine("6. Modify Flight Details");
+        Console.WriteLine("7. Display Flight Schedule");
+        Console.WriteLine("0. Exit");
+        Console.WriteLine("Please select your option: ");
+    }
+
+
+
+
+    // MAIN LOOP 
+
+    // NOTE FOR FUTURE REFERENCE : 
+    // WHEN LOADING THE FILES, NEED TO PUT "FINISHED LOADING AND DISPLAY QUANTITY OF THINGS LOADED, THEN HAVE A GAP
+    // BEFORE THE MAIN MENU 
+
 
